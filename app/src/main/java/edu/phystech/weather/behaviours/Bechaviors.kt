ac031c6d959f575c5cfc204b869005b964af8763
NikeVas <vasilenko.na@phystech.edu>
2022-05-15 22:42:14 +0300

    Fix collapsing toolbar

--- app/src/main/java/edu/phystech/weather/behaviours/Bechaviors.kt
+++ app/src/main/java/edu/phystech/weather/behaviours/Bechaviors.kt
@@ -7,10 +7,15 @@ import android.view.View
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.appbar.AppBarLayout
 import kotlin.properties.Delegates
 
+
 class AvatarImageBehavior(context: Context?, attrs: AttributeSet?) :
-    CoordinatorLayout.Behavior<TextView?>(context, attrs) {
+    CoordinatorLayout.Behavior<View?>(context, attrs) {
+
+
 
     var finalXPosition by Delegates.notNull<Float>()
     var finalYPosition by Delegates.notNull<Float>()
@@ -27,27 +32,29 @@ class AvatarImageBehavior(context: Context?, attrs: AttributeSet?) :
 
     override fun onDependentViewChanged(
         parent: CoordinatorLayout,
-        avatar: TextView,
+        avatar: View,
         dependency: View
     ): Boolean {
-        val toolbar_height = parent.findViewById<Toolbar>(R.id.toolbar).height
-        val height = dependency.height
-        val current = dependency.bottom.toFloat()
-
-        var progress = (current - toolbar_height) / (height - toolbar_height)
-        if (progress < 0) {
-            progress = 0F
-        }
-        progress = 1 - progress
-
-        calculatePosition(avatar, progress)
-
+//        val toolbar_height = parent.findViewById<Toolbar>(R.id.toolbar).height
+//        val height = dependency.height
+//        val current = dependency.bottom.toFloat()
+//
+//        var progress = (current - toolbar_height) / (height - toolbar_height)
+//        if (progress < 0) {
+//            progress = 0F
+//        }
+//        progress = 1 - progress
+//
+//        calculatePosition(avatar, progress)
+//
         return true
     }
 
-    private fun calculatePosition(avatar: TextView, progress : Float) {
+
+    private fun calculatePosition(avatar: View, progress : Float) {
         avatar.setY((finalYPosition - startYPosition) * progress + startYPosition)
         avatar.setX((finalXPosition - startXPosition) * progress + startXPosition)
+        Log.e("aboba", avatar.x.toString() + " " +  avatar.y.toString())
     }
 
 }
